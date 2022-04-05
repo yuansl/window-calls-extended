e9f04eed0dab065fc8842e5708e78f21eb1af322
Hendrik G. Seliger <github@hseliger.eu>
2022-04-05 13:46:40 +0200

    Initial extended version

--- extension.js
+++ extension.js
@@ -16,24 +16,38 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+// Created: 2022-04-05T09:02:24+02:00 by Hendrik G. Seliger (github@hseliger.eu)
+// Last changes: 2022-04-05T13:09:46+02:00 by Hendrik G. Seliger (github@hseliger.eu)
+
+// Based on the initial version by ickyicky (https://github.com/ickyicky),
+// extended by a few methods to provide the focused window's title, window class, and pid.
+
 /* exported init */
 
 const { Gio } = imports.gi;
 
 const MR_DBUS_IFACE = `
 <node>
-    <interface name="org.gnome.Shell.Extensions.Windows">
+    <interface name="org.gnome.Shell.Extensions.WindowsExt">
         <method name="List">
             <arg type="s" direction="out" name="win"/>
+        </method>
+        <method name="FocusTitle">
+            <arg type="s" direction="out" />
+        </method>
+        <method name="FocusPID">
+            <arg type="s" direction="out" />
+        </method>
+        <method name="FocusClass">
+            <arg type="s" direction="out" />
         </method>
     </interface>
 </node>`;
 
-
 class Extension {
     enable() {
         this._dbus = Gio.DBusExportedObject.wrapJSObject(MR_DBUS_IFACE, this);
-        this._dbus.export(Gio.DBus.session, '/org/gnome/Shell/Extensions/Windows');
+        this._dbus.export(Gio.DBus.session, '/org/gnome/Shell/Extensions/WindowsExt');
     }
 
     disable() {
@@ -44,9 +58,42 @@ class Extension {
     List() {
         let win = global.get_window_actors()
             .map(a => a.meta_window)
-            .map(w => ({ class: w.get_wm_class(), pid: w.get_pid(), id: w.get_id(), maximized: w.get_maximized(), focus: w.has_focus(), title: w.get_title()}));
+            .map(w => ({ class: w.get_wm_class(), pid: w.get_pid(), id: w.get_id(), maximized: w.get_maximized(), focus: w.has_focus(), title: w.get_title() }));
         return JSON.stringify(win);
     }
+    FocusTitle() {
+        let win = global.get_window_actors()
+            .map(a => a.meta_window)
+            .map(w => ({ focus: w.has_focus(), title: w.get_title() }));
+        for (let [_ignore , aWindow] of win.entries()) {
+            let [focus,theTitle] = Object.entries(aWindow);
+            if (focus[1] == true )
+                return theTitle[1];
+        }
+        return "";
+    }
+    FocusPID() {
+        let win = global.get_window_actors()
+            .map(a => a.meta_window)
+            .map(w => ({ focus: w.has_focus(), pid: w.get_pid() }));
+            for (let [_ignore , aWindow] of win.entries()) {
+                let [focus,thePID] = Object.entries(aWindow);
+                if (focus[1] == true )
+                    return ""+thePID[1]; // Turn number into string
+            }
+            return "";
+        }
+    FocusClass() {
+        let win = global.get_window_actors()
+            .map(a => a.meta_window)
+            .map(w => ({ focus: w.has_focus(), class: w.get_wm_class() }));
+            for (let [_ignore , aWindow] of win.entries()) {
+                let [focus,theClass] = Object.entries(aWindow);
+                if (focus[1] == true )
+                    return theClass[1];
+            }
+            return "";
+        }
 }
 
 function init() {
