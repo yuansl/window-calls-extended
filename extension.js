011c0b0d83c9a690e480629644fa10739a57fd89
yuansl <yuanshenglong@126.com>
2024-03-24 11:38:02 +0800

    add RaiseEmacsWindow method

--- extension.js
+++ extension.js
@@ -28,6 +28,8 @@
 /* exported init */
 
 import Gio from 'gi://Gio';
+import Mtk from 'gi://Mtk';
+import Clutter from 'gi://Clutter';
 
 const MR_DBUS_IFACE = `
 <node>
@@ -44,6 +46,10 @@ const MR_DBUS_IFACE = `
         <method name="FocusClass">
             <arg type="s" direction="out" />
         </method>
+        <method name="RaiseEmacsWindow">
+            <arg type="s" direction="out" />
+        </method>
+
     </interface>
 </node>`;
 
@@ -51,6 +57,7 @@ export default class WCExtension {
     enable() {
         this._dbus = Gio.DBusExportedObject.wrapJSObject(MR_DBUS_IFACE, this);
         this._dbus.export(Gio.DBus.session, '/org/gnome/Shell/Extensions/WindowsExt');
+	this._seat = Clutter.get_default_backend().get_default_seat();
     }
 
     disable() {
@@ -79,22 +86,46 @@ export default class WCExtension {
         let win = global.get_window_actors()
             .map(a => a.meta_window)
             .map(w => ({ focus: w.has_focus(), pid: w.get_pid() }));
-            for (let [_ignore , aWindow] of win.entries()) {
-                let [focus,thePID] = Object.entries(aWindow);
-                if (focus[1] == true )
-                    return ""+thePID[1]; // Turn number into string
-            }
-            return "";
+        for (let [_ignore , aWindow] of win.entries()) {
+            let [focus,thePID] = Object.entries(aWindow);
+            if (focus[1] == true )
+                return ""+thePID[1]; // Turn number into string
         }
+        return "";
+    }
     FocusClass() {
         let win = global.get_window_actors()
             .map(a => a.meta_window)
             .map(w => ({ focus: w.has_focus(), class: w.get_wm_class() }));
-            for (let [_ignore , aWindow] of win.entries()) {
-                let [focus,theClass] = Object.entries(aWindow);
-                if (focus[1] == true )
-                    return theClass[1];
-            }
-            return "";
+        for (let [_ignore , aWindow] of win.entries()) {
+            let [focus,theClass] = Object.entries(aWindow);
+            if (focus[1] == true )
+                return theClass[1];
         }
+        return "";
+    }
+
+    _raise_window(window) {
+        const activeWorkspace = global.workspaceManager.get_active_workspace();
+        window.change_workspace(activeWorkspace);
+        window.get_workspace().activate_with_focus(window, true)
+        window.activate(0)
+        const win_rect = window.get_frame_rect()
+        const [x, y] = global.get_pointer()
+        const pointer_rect = new Mtk.Rectangle({ x, y, width: 1, height: 1 })
+        if (!pointer_rect.intersect(win_rect)[0]) {
+            const { x, y, width, height } = win_rect
+            this._seat.warp_pointer(x + width / 2, y + height / 2)
+        }
+    }
+
+    RaiseEmacsWindow() {
+	let win = global.get_window_actors()
+            .map(a => a.meta_window)
+            .filter(w => w.get_wm_class() === 'emacs');
+        for (let [_ignore , window] of win.entries()) {
+	    this._raise_window(window);
+	}
+	return "";
+    }
 }
